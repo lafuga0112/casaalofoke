b952c5c6bb85806ef6b8169b787132e83ad4cc0d
lafuga0112 <50000949+lafuga0112@users.noreply.github.com>
2025-08-16 22:44:35 -0400

    ddd

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   
   // Servidor
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     env: process.env.NODE_ENV || 'development'
   },
   
